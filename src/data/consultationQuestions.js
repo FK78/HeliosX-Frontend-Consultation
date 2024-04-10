0b76d452323c38814e03ed0ebc2f85647aa462b3
FK78 <34196381+FK78@users.noreply.github.com>
2024-04-10 01:44:52 +0100

    add data for questions

---
+++ src/data/consultationQuestions.js
@@ -0,0 +1,29 @@
+const consultationQuestions = [
+    {
+        question: "Are you aged between 18 and 65?",
+        questionName: "Question1",
+        answered: false,
+    },
+    {
+        question: "Do you know how and when to use your inhaler?",
+        questionName: "Question2",
+        answered: false
+    },
+    {
+        question: "Have you called an ambulance or had emergency treatment for breathing problems in the last year?",
+        questionName: "Question3",
+        answered: false
+    },
+    {
+        question: "Do you use your inhaler more than four times a day?",
+        questionName: "Question4",
+        answered: false
+    },
+    {
+        question: "Have you been diagnosed with asthma by your doctor or nurse?",
+        questionName: "Question5",
+        answered: false
+    }
+]
+
+export default consultationQuestions
