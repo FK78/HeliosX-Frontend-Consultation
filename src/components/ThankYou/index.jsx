3645d35edf48936e58f697932014b08bd66000e3
FK78 <34196381+FK78@users.noreply.github.com>
2024-04-10 02:29:17 +0100

    create ThankYou component

---
+++ src/components/ThankYou/index.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+const ThankYou = () => {
+    return (
+        <div className='thankYouWrapper'>
+            <h2 className='header'>Thank You!</h2>
+        </div>
+    )
+}
+
+export default ThankYou
