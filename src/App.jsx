a833c5130cd584f4c4f1d1c349a947d6c38a97af
FK78 <34196381+FK78@users.noreply.github.com>
2024-04-10 01:45:53 +0100

    create ConsultationForm and functions to handle answers in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,79 @@
-function App() {
-  return <></>;
-}
+import { useEffect, useState } from "react";
+import ConsultationForm from "./components/ConsultationForm";
+import consultationService from "./services/consultation";
+import consultationQuestions from "./data/consultationQuestions";
+
+const App = () => {
+  const [answers, setAnswers] = useState({});
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  useEffect(() => {
+    setQuestions(consultationQuestions);
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    consultationService.sendConsultationAnswers(answers);
+  };
+
+  const updateQuestion = (name) => {
+    const currentQuestion = questions.find(
+      ({ questionName }) => questionName === name
+    )?.question;
+
+    const currentQuestionName = questions.find(
+      ({ questionName }) => questionName === name
+    )?.questionName;
+
+    const updatedQuestion = {
+      question: currentQuestion,
+      questionName: currentQuestionName,
+      answered: true,
+    };
+
+    setQuestions(
+      questions.map((question) =>
+        question.questionName !== name ? question : updatedQuestion
+      )
+    );
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    
+    const getQuestion = questions.find(
+      ({ questionName }) => questionName === name
+    ).question;
+    
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [getQuestion]: value,
+    }));
+
+    updateQuestion(name);
+
+    const hasTheCurrentOneAlreadyBeenAnswered = questions.find(
+      ({ questionName }) => questionName === name
+    ).answered;
+
+    if (hasTheCurrentOneAlreadyBeenAnswered === false) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+  };
+
+  const questionsToPass = questions.filter(
+    (question, index) => index === currentQuestionIndex || question.answered
+  );
+
+  return (
+    <div className="contentWrapper">
+      <ConsultationForm
+        onSubmit={handleSubmit}
+        onChange={handleInputChange}
+        questions={questionsToPass}
+      />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/ConsultationForm/index.jsx
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+import RadioButton from "../RadioButton";
+
+const ConsultationForm = ({ onSubmit, onChange, questions }) => {
+  return (
+    <form className="consultationForm" onSubmit={onSubmit}>
+      {questions.map((q, i) => (
+        <div className="consultationQuestion" key={q.questionName}>
+          <div className="question">{q.question}</div>
+          <div className="radioButtonContainer">
+            <RadioButton
+              name={q.questionName}
+              id={`Yes${i}`}
+              label="Yes"
+              value="Yes"
+              onChange={onChange}
+            />
+            <RadioButton
+              name={q.questionName}
+              id={`No${i}`}
+              label="No"
+              value="No"
+              onChange={onChange}
+            />
+          </div>
+        </div>
+      ))}
+      <div className="submitButtonWrapper">
+        <button className="submitButton" type="submit">Submit</button>
+      </div>
+    </form>
+  );
+};
+
+ConsultationForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  questions: PropTypes.array.isRequired,
+};
+
+export default ConsultationForm;
